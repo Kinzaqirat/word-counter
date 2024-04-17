1e99bee18b06129232b82693ebc8250f9408ca76
Qiratsumra <saeedqirat43@gmail.com>
2024-04-17 16:53:20 +0500

    word counter

---
+++ main.ts
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+
+// import inquirer for getting input from user
+import inquirer from "inquirer";
+// code for show output
+const answers : {Sentence :string }= await inquirer.prompt([{
+    name :"Sentence",
+    type :"input",
+    message:"Enter your sentence to count words."
+}]);
+// adding trim for remove spaces and split to cound words
+const words = answers.Sentence.trim().split(" ");
+// print it and add lenght for count lenght
+console.log(`Your sentence has ${words.length} words.`);
+
